ddbb1099299dd30024c79be6e19ac90bc12e6112
delaneyg25 <55740783+delaneyg25@users.noreply.github.com>
2019-09-30 05:03:48 -0400

    Timer function and additional comments added

--- js/script.js
+++ js/script.js
@@ -5,28 +5,28 @@ var quotes = [
    Author: 'Mark Twain',
    Citation: 'Notebook entry, January or February 1894, Mark Twain\'s Notebook, ed. Albert Bigelow Paine',
    Year: '1935',
-   Tags: ['Wisdom'],
+   Tags: null,
   },
   {
    Quote: 'Life is like riding a bicycle. To keep your balance you must keep moving.',
    Author: 'Albert Einstein',
    Citation: 'Letter to his son Eduard',
    Year: '1930',
-   Tags: ['Wisdom'],
+   Tags: null,
   },
   {
    Quote: 'Our virtues and our failings are inseparable, like force and matter. When they separate, man is no more.',
    Author: 'Nikola Tesla',
    Citation: '"The Problem of Increasing Human Energy", Century Illustrated Magazine',
    Year: '1930',
-   Tags: ['Wisdom'],
+   Tags: null,
   },
   {
    Quote: 'I haven\'t a particle of confidence in a man who has no redeeming petty vices whatsoever.',
    Author: 'Mark Twain',
    Citation: '"Answers to Correspondents", The Californian, 17 June 1865. Anthologized in The Celebrated Jumping Frog of Calaveras County, and Other Sketches',
    Year: '1867',
-   Tags: ['Humor', 'Wisdom'],
+   Tags: ['Humor'],
   },
   {
    Quote: 'All of science is nothing more than the refinement of everyday thinking.',
@@ -40,21 +40,21 @@ var quotes = [
    Author: 'Nikola Tesla',
    Citation: '"A Visit to Nikola Tesla" by Dragislav L. Petković in Politika',
    Year: '1927',
-   Tags: ['Wisdom', 'Politics'],
+   Tags: ['Politics'],
   },
   {
    Quote: 'All you need in this life is ignorance and confidence, and then Success is sure.',
    Author: 'Mark Twain',
    Citation: 'Mark Twain\'s Notebook',
    Year: '1887',
-   Tags: ['Humor', 'Wisdom'],
+   Tags: ['Humor'],
   },
   {
    Quote: 'Never do anything against conscience even if the state demands it.',
    Author: 'Albert Einstein',
    Citation: 'as quoted by Virgil Henshaw in Albert Einstein: Philosopher Scientist',
    Year: '1949',
-   Tags: ['Wisdom', 'Politics'],
+   Tags: ['Politics'],
   },
   {
    Quote: 'Something dreadful happens to students between first and twelfth grades, and it\'s not just puberty.',
@@ -73,12 +73,11 @@ var quotes = [
 ]
 
 var newColor = '';
-
+var interval;
 
 function getRandomQuote() {
   //Create a random integer from 0 and the total number of quotes (currently 10) - 1 .
   var randomInt = Math.floor(Math.random() * quotes.length);
-  console.log(randomInt);
 
   //Use the random iniger to select a quote from the array
   for (i=0; i <= quotes.length;  i += 1) {
@@ -87,12 +86,13 @@ function getRandomQuote() {
     } 
   }
 }
-
+//Create a random number for an RGB value
 function colorMath() {
   return Math.floor(Math.random() * 256 );
  }
  
- function randomColor() {
+//Create the string required to set a new RGB value
+function randomColor() {
   var theColor =  'rgb(';
   theColor += colorMath() + ',';
   theColor += colorMath() + ',';
@@ -100,6 +100,7 @@ function colorMath() {
   return theColor;
  }
 
+//Function to call and print a new quote and change colors
 function printQuote() {
 
   var theQuote = getRandomQuote();
@@ -116,15 +117,25 @@ function printQuote() {
     quoteBox += '<span class="year">' + theQuote.Year + '</span>';
   }
 
+  if (theQuote.Tags !== null) {    
+    quoteBox += '<span class="tags">' + theQuote.Tags + '</span>';
+  }
+
   quoteBox += '</p>';
   
+  //print the random quote
   var printDiv = document.getElementById("quote-box")
   printDiv.innerHTML = quoteBox;
 
+  //change the background and color styles
   document.body.style.backgroundColor = randomColor()
   document.body.style.color = randomColor()
+
 }
 
+//Automatically change the quote and colors after 20 seconds
+interval = setInterval(printQuote, 20000)
 
-// When the "Show another quote" button is clicked, call the `printQuote` function.
+
+// Change the quote and colors when "Show Another Quote" button is clicked
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
